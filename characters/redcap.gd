e45e1a2604ef6e9532eaa61cec8e3373bab67a9c
Addison Mink <addison.mink@example.com>
2025-02-02 12:36:23 -0600

    Implement redcap behavior.

--- characters/redcap.gd
+++ characters/redcap.gd
@@ -2,45 +2,93 @@ extends CreatureBody
 
 const Facing = preload("res://scripts/facing.gd")
 
-const WALK_SPEED = 100
+const WALK_SPEED = 200
+const ATTACK_WINDUP_DURATION = 0.25
+const ATTACK_COOLDOWN_DURATION = 1
+const ATTACK_RANGE_TOLERANCE = 16
+const ATTACK_RANGE = 16 * 8
+const ATTACK_SPEED = 400
 
 enum State {
 	IDLE,
 	WALKING,
+	ATTACK_WINDUP,
+	ATTACKING,
 } 
 
 @export var player: CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 var state: State = State.IDLE
 var facing: Facing.Facing = Facing.Facing.DOWN
+var attack_windup_timer: Timer = null
+var attack_cooldown_timer: Timer = null
+
 
 func _ready() -> void:
 	super._ready()
-	transitionToIdle()
+	attack_windup_timer = make_timer(ATTACK_WINDUP_DURATION)
+	attack_cooldown_timer = make_timer(ATTACK_COOLDOWN_DURATION)
+	transition_to_idle()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if(isStunned()):
 		return		
 	match state:
-		State.IDLE: updateIdle()
-		State.WALKING: updateWalking()
+		State.IDLE: update_idle()
+		State.WALKING: update_walking()
+		State.ATTACK_WINDUP: update_attack_windup()
+		State.ATTACKING: update_attacking()
 
-func transitionToIdle():
+func transition_to_idle():
 	state = State.IDLE
 	velocity = Vector2.ZERO
 	Facing.play_animation_facing(animated_sprite, "idle", facing)
 	
-func updateIdle():
+func update_idle():
 	if not player:
 		return
-	
-	transitionToWalking()
+	transition_to_walking()
 
-func transitionToWalking():
+func transition_to_walking():
 	state = State.WALKING
-	updateWalking()
+	update_walking()
 	
-func updateWalking():
-	velocity = (player.position - position).normalized() * WALK_SPEED
+func update_walking():	
 	facing = Facing.facing_to(position, player.position)
-	Facing.play_animation_facing(animated_sprite, "walk", facing)
+	var target = Facing.facing_vector(facing) * -1 * ATTACK_RANGE + player.position			
+	var diff_to_target = target - position
+	
+	if attack_cooldown_timer.get_time_left() <= 0 and diff_to_target.length() <= ATTACK_RANGE_TOLERANCE:
+		transition_to_attack_windup()
+	else:
+		velocity = diff_to_target.normalized() * WALK_SPEED		
+		Facing.play_animation_facing(animated_sprite, "walk", facing)
+	
+func transition_to_attack_windup():
+	state = State.ATTACK_WINDUP
+	velocity = Vector2.ZERO
+	attack_windup_timer.start()
+	Facing.play_animation_facing(animated_sprite, "attack_windup", facing)	
+
+func update_attack_windup():
+	if attack_windup_timer.get_time_left() <= 0:
+		transition_to_attacking()
+		
+func transition_to_attacking():
+	state = State.ATTACKING
+	velocity = Facing.facing_vector(facing) * ATTACK_SPEED
+	Facing.play_animation_facing(animated_sprite, "attack", facing)		
+	
+func update_attacking():
+	if animated_sprite.frame_progress >= 1:
+		attack_cooldown_timer.start()
+		transition_to_idle()	
+
+# UTIL
+func make_timer(wait_time: float) -> Timer:
+	var timer = Timer.new()
+	timer.one_shot = true
+	timer.wait_time = wait_time
+	add_child(timer)
+	return timer
